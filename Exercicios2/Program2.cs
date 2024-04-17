a43063f16735d6173a85a988a4bcbcfc96122b89
TataDevWitcher <vitorialadyletal@gmail.com>
2024-04-17 00:01:03 -0300

    Segundo exercício

---
+++ Exercicios2/Program2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio2
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*Área de um círculo*");
+            Console.WriteLine("Obs.: Use o . como separador de casas decimais");
+
+            Console.WriteLine("Entre com o raio do círculo:");
+            double raioCirculo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double pi = 3.14159;
+            double areaCirculo = pi * (Math.Pow(raioCirculo, 2));
+
+            Console.WriteLine($"A área do círculo é: {areaCirculo:F4}");
+        }
+    }
+}
