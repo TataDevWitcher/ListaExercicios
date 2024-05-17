3e47033586f33e18ed5225c671adc1fdf7ba0420
TataDevWitcher <vitorialadyletal@gmail.com>
2024-05-16 22:14:10 -0300

    Adição certa Exercício Valor Produto - Orientação à objetos

---
+++ Exercicio - Orientacao_Objetos_Valor_Produtos/Produto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExercicioValorProdutos
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public double AdicionarProdutos(int quantidade)
+        {
+            return Quantidade += quantidade;
+        }
+
+        public double RemoverProdutos(int quantidade)
+        {
+            return Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", R$"
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade.ToString("F2", CultureInfo.InvariantCulture)
+                + " unidades, Total:"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicio - Orientacao_Objetos_Valor_Produtos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioValorProdutos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*Cálculo valores dos produtos*");
+            Console.WriteLine("Obs.: Use a vírgula como separador de casas decimais");
+
+            Produto produto = new Produto();
+
+            Console.WriteLine("Insira o nome do produto, o preço e a quantidade respectivamente separados por um espaço:");
+            string[] dadosProduto = Console.ReadLine().Split(' ');
+            produto.Nome = dadosProduto[0];
+            produto.Preco = double.Parse(dadosProduto[1]);
+            produto.Quantidade = int.Parse(dadosProduto[2]);
+
+            Console.WriteLine("Dados do produto: " + produto);
+            //Consigo agora chamar a variável sem problemas por causa da implementação do método override na classe Produto
+
+            Console.WriteLine("");
+            Console.WriteLine("Entrada no estoque, informe a quantidade adicionada:");
+            int qte = int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos(qte);
+            Console.WriteLine($"Dados atualizados: {produto}");
+
+            Console.WriteLine("");
+            Console.WriteLine("Saída no estoque, informe a quantidade removida:");
+            qte = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(qte);
+            Console.WriteLine($"Dados atualizados: {produto}");
+        }
+    }
+}
